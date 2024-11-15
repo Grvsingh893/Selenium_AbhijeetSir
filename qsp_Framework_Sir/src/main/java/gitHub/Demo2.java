128f487469434f3bba1a0ae518c06159ed6cf2eb
grvsi <grvsi@Gaurav>
2024-11-15 13:54:01 +0530

    Practise

--- qsp_Framework_Sir/src/main/java/gitHub/Demo2.java
+++ qsp_Framework_Sir/src/main/java/gitHub/Demo2.java
@@ -3,14 +3,13 @@ package gitHub;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class GitHub2 {
+public class Demo2 {
 	@Test
-	public void deleteMessage() {
-		Reporter.log("Welcome to GitHub", true);
-	}
-
+public void deleteMessage() {
+	Reporter.log("Welcome to GitHub", true);
+}
 	@Test
 	public void mergeMessage() {
 		Reporter.log("Welcome to GitHub", true);
-	}
+}
 }
